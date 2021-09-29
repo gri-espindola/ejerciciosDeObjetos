bf7385decc80d1c1613314fe507c50de65b7a255
gri-espindola <griespindola@hotmail.com>
2021-09-28 21:07:44 -0300

    subiendo ejercicios

---
+++ objetos.js
@@ -0,0 +1,60 @@
+/* const user = {
+    firtsName: "Griselda",
+    lastName: "Espindola",
+    email: "griespindola27@gmail.com",
+    age: 29
+}
+
+console.log (`Hola, mi nombre es ${user.firtsName} y tengo ${user.age} años`) */
+
+//EJERCICIO 2
+
+const datosUno = {
+    id: 1,
+    nombre: "Ada Lovelace",
+    email: "ada@gmail.com",
+    telefono: "1234567890"
+}
+
+const datosDos = {
+    id: 2,
+    nombre: "Grace Hopper",
+    email: "grace@hotmail.com",
+    telefono: "0987654321"
+}
+
+const datosTres = {
+    id: 3,
+    nombre: "Hedy Lamarr",
+    email: "hedy@gmail.com",
+    telefono: "6789054321"
+}
+ 
+
+const baseDeDatos = {
+    datosUno,
+    datosDos,
+    datosTres
+}
+
+console.log(baseDeDatos)
+
+
+
+//EJERCICIO 5
+/* 
+Crear un array llamado tecnologiasConocidas donde se listen las tecnologias que aprendiste durante el curso y las que ya supieras de antes
+
+Agregar al objeto creado en el ejercicio 1 dos propiedades:
+
+sabeProgramar: true
+tecnologiasConocidas: [el array que creaste recien] */
+
+/* const usuarioEjemplo = {
+    id: 0,
+    nombre: "ejemplo",
+    email: "ejemploterra.com.ar"
+    telefono: "375654654654"
+}
+
+const tecnologiasConocidas */
